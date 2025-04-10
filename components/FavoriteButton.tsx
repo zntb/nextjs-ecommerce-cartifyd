1f057a775e4b00d76c411d44fda1184a290bed41
zntb <zenetibi@yahoo.com>
2025-04-10 10:22:46 +0300

    Responsive Header, Footer

---
+++ components/FavoriteButton.tsx
@@ -0,0 +1,31 @@
+// import { Product } from '@/sanity.types';
+import { Heart } from 'lucide-react';
+import Link from 'next/link';
+import React from 'react';
+
+const FavoriteButton = ({
+  showProduct = false,
+}: //   product,
+{
+  showProduct?: boolean;
+  //   product?: Product | null | undefined;
+}) => {
+  return (
+    <>
+      {!showProduct ? (
+        <Link href={'/wishlist'} className='group relative'>
+          <Heart className='w-5 h-5 hover:text-shop_light_green hoverEffect' />
+          <span className='absolute -top-1 -right-1 bg-shop_dark_green text-white h-3.5 w-3.5 rounded-full text-xs font-semibold flex items-center justify-center'>
+            0
+          </span>
+        </Link>
+      ) : (
+        <button className='group relative hover:text-shop_light_green hoverEffect border border-shop_light_green/80 hover:border-shop_light_green p-1.5 rounded-sm'>
+          <Heart className='text-shop_light_green/80 group-hover:text-shop_light_green hoverEffect mt-.5 w-5 h-5' />
+        </button>
+      )}
+    </>
+  );
+};
+
+export default FavoriteButton;
