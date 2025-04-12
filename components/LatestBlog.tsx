71e847e3ef73807002ea07375808218eb6728022
zntb <zenetibi@yahoo.com>
2025-04-12 10:00:20 +0300

    Add Dynamic Shop Page Setup and Filtering

--- components/LatestBlog.tsx
+++ components/LatestBlog.tsx
@@ -6,10 +6,9 @@ import { urlFor } from '@/sanity/lib/image';
 import Link from 'next/link';
 import { Calendar } from 'lucide-react';
 import dayjs from 'dayjs';
-import { Blog } from '@/sanity.types';
 
 const LatestBlog = async () => {
-  const blogs: Blog[] = await getLatestBlogs();
+  const blogs = await getLatestBlogs();
   return (
     <div className='mb-10 lg:mb-20'>
       <Title>Latest Blog</Title>
@@ -35,7 +34,7 @@ const LatestBlog = async () => {
                       key={index}
                       className='font-semibold text-shop_dark_green tracking-wider'
                     >
-                      {item?._key}
+                      {item?.title}
                     </p>
                   ))}
                   <span className='absolute left-0 -bottom-1.5 bg-lightColor/30 inline-block w-full h-[2px] group-hover:bg-shop_dark_green hover:cursor-pointer hoverEffect' />
